9e8669b3a2a075f0b877f0d2791af84f1d5fa00a
jessonliu <jessonliu0705@163.com>
2016-07-01 11:27:21 +0800

    Initial Commit

---
+++ JFUpLoadVideo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JFUpLoadVideo
+//
+//  Created by iOS-Developer on 16/7/1.
+//  Copyright © 2016年 Jessonliu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
